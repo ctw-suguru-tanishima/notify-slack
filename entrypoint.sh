d2dcd8949720c9636001e5564ed4eef8f553752b
suguru.tanishima <suguru.tanishima@claytechworks.jp>
2021-10-27 15:35:41 +0900

    notify slack.

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -XPOST -d "token=${INPUT_TOKEN}" \
+    -d "channel=${INPUT_CHANNEL}" \
+    -d "icon_emoji=${INPUT_ICON_EMOJI}" \
+    -d "username=${INPUT_USERNAME}" \
+    -d "text= ${INPUT_TEXT}" \
+    -d "attachments=[{\"color\":\"${INPUT_ATTACHMENTS_COLOR}\",\"text\":\"${INPUT_ATTACHMENTS_TEXT}\"}]" \
+    "https://slack.com/api/chat.postMessage"
+ 
